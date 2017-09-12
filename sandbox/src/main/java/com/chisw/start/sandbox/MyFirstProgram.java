0b12db13c6adf01edb00cfe15c20952f5d5354f7
Tarrest <reva.htp@gmail.com>
2017-09-12 15:03:45 +0300

    работа с фуекциями примитив

--- sandbox/src/main/java/com/chisw/start/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/chisw/start/sandbox/MyFirstProgram.java
@@ -3,6 +3,32 @@ package com.chisw.start.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World1!");
+        hello();
+        hello("Test"); //вызов функции с параметром
+        System.out.println(area(8.90));
+        double a1 = 5;
+        double b1 = 7;
+        System.out.println(area(a1,b1));
     }
+
+    public static void hello() {
+        System.out.println("Hello, " + "World2!");
+    }
+
+    public static void hello(String smth) {
+        System.out.println("Hello, " + smth);
+    }
+
+    public static double area(double l) {
+        return l*l;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
+    }
+
+
+
+
 }
