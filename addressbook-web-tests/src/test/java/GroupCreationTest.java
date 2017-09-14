202cbd85f15944b8053506080669502a09447399
Tarrest <reva.htp@gmail.com>
2017-09-14 13:20:49 +0300

    добавили параметры вместо конкретных значений

--- addressbook-web-tests/src/test/java/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/GroupCreationTest.java
@@ -1,18 +1,12 @@
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTest {
     WebDriver driver;
@@ -25,40 +19,40 @@ public class GroupCreationTest {
         driver = new FirefoxDriver(capabilitiesFirefox);
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.get("http://localhost/addressbook/");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String login, String password) {
         driver.findElement(By.name("user")).click();
         driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("user")).sendKeys(login);
         driver.findElement(By.name("pass")).click();
         driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.name("pass")).sendKeys(password);
         driver.findElement(By.id("LoginForm")).click();
     }
 
     @Test
     public void testGroupCreation() {
         createNewGroup();
-        fillAndSubmitGroupForm();
-        checkGroupCreation();
+        fillAndSubmitGroupForm("Evan Warner", "test1", "test2");
+        returnToGroupPage();
     }
 
-    private void checkGroupCreation() {
+    private void returnToGroupPage() {
         driver.findElement(By.linkText("GROUPS")).click();
     }
 
-    private void fillAndSubmitGroupForm() {
+    private void fillAndSubmitGroupForm(String name, String header, String footer) {
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("Evan Warner");
+        driver.findElement(By.name("group_name")).sendKeys(name);
         driver.findElement(By.name("group_header")).click();
         driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("test1");
+        driver.findElement(By.name("group_header")).sendKeys(header);
         driver.findElement(By.name("group_footer")).click();
         driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("test2");
+        driver.findElement(By.name("group_footer")).sendKeys(footer);
         driver.findElement(By.name("submit")).click();
     }
 
