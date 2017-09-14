df87624fbfa9abf031735b20b4dcedce0040f084
Tarrest <reva.htp@gmail.com>
2017-09-14 11:09:28 +0300

    код до рефакторинга

---
+++ addressbook-web-tests/src/test/java/GroupCreationTest.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class GroupCreationTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        DesiredCapabilities capabilitiesFirefox = new DesiredCapabilities();
+        capabilitiesFirefox.setCapability("marionette", true);
+        System.setProperty("webdriver.gecko.driver", "c:\\Geckodriver\\geckodriver.exe");
+        driver = new FirefoxDriver(capabilitiesFirefox);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.get("http://localhost/addressbook/");
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.id("LoginForm")).click();
+    }
+    
+    @Test
+    public void testGroupCreation() {
+        driver.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        driver.findElement(By.linkText("GROUPS")).click();
+        driver.findElement(By.name("new")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).clear();
+        driver.findElement(By.name("group_name")).sendKeys("Evan Warner");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).clear();
+        driver.findElement(By.name("group_header")).sendKeys("test1");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).clear();
+        driver.findElement(By.name("group_footer")).sendKeys("test2");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("GROUPS")).click();
+    }
+
+}
